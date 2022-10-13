6e5b20520683757269ab32e4c0417d31d636b877
Pascal Stepien <pstepien@hotmail.co.uk>
2022-10-13 20:19:29 +0100

    split in to components and added tailwind

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,14 +1,18 @@
+<?php
+    $route = Route::current()->uri();
+?>
+
 <!-- navbar -->
 <nav class="navbar navbar-expand-lg bg-light">
     <div class="container-fluid">
-        <div class="navbar-nav">
-            <a class="nav-link active" aria-current="page" href="/">Home</a>
-            <a class="nav-link" href="/projects">Projects</a>
-            <a class="nav-link" href="/experience">Experience</a>
-            <a class="nav-link" href="/academia">Academia</a>
-            <a class="nav-link" href="/cv">CV</a>
-            <a class="nav-link" href="/about">About</a>
-            <a class="nav-link" href="/contact">Contact</a>
+        <div class="navbar-nav ml-auto sm:flex-row">
+            <a class="nav-link {{ $route == "/" ? "active" : "" }}" aria-current="page" href="/">Home</a>
+            <a class="nav-link {{ $route == "projects" ? "active" : "" }}" href="/projects">Projects</a>
+            <a class="nav-link {{ $route == "experience" ? "active" : "" }}" href="/experience">Experience</a>
+            <a class="nav-link {{ $route == "academia" ? "active" : "" }}" href="/academia">Academia</a>
+            <a class="nav-link {{ $route == "cv" ? "active" : "" }}" href="/cv">CV</a>
+            <a class="nav-link {{ $route == "about" ? "active" : "" }}" href="/about">About</a>
+            <a class="nav-link {{ $route == "contact" ? "active" : "" }}" href="/contact">Contact</a>
         </div>
     </div>
 </nav>
